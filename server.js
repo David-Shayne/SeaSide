b883c7a0b50b6ad83c7d11f5f2002c1562a489c4
unknown <davidmshayne.jftd@gmail.com>
2020-07-20 13:19:45 +0200

    initial

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose
     .then(console.log('MongoDB Connected'));
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
